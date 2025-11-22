0143de4eabfb6bc7c509f7bfe2e6b72856e46ed8
ankurjha2006 <kumarankur7258@gmail.com>
2025-11-22 22:59:54 +0530

    password generate only numbers

--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -56,13 +56,8 @@ new class extends Component {
 
     private function generateRandomPassword($length = 7)
     {
-        // Generates a random 7-character alphanumeric password
-        $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-        $password = '';
-        for ($i = 0; $i < $length; $i++) {
-            $password .= $chars[random_int(0, strlen($chars) - 1)];
-        }
-        return $password;
+        $numbers = '0123456789';
+    return substr(str_shuffle(str_repeat($numbers, $length)), 0, $length);
     }
 
     public function register(): void
